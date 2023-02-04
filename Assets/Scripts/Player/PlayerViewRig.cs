52e4f556b147ca4b5d3f2b2f83f9c8daea9164a8
Ben Clothier <ben.clothier8@icloud.com>
2023-02-04 11:16:56 +0000

    Install coherance and implement camera movement

--- Assets/Scripts/Player/PlayerViewRig.cs
+++ Assets/Scripts/Player/PlayerViewRig.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerViewRig : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 2.0f; // Speed of movement along the forward axis
+    public float lerpSpeed = 3.0f; // Speed of the lerp animation
+
+    private Vector3 targetPosition; // Target position to move towards
+
+    private void Awake()
     {
-        
+        targetPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            targetPosition = targetPosition + transform.forward * moveSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            targetPosition = targetPosition - transform.forward * moveSpeed * Time.deltaTime;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, lerpSpeed * Time.deltaTime);
     }
 }
